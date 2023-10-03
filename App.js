f29caf2827444b9bf9cb4f665e86a402606240f3
Aditya Dhanraj <opsingh861@gmail.com>
2023-10-03 20:58:39 +0530

    Styling using inline, internal and external

--- App.js
+++ App.js
@@ -1,24 +1,36 @@
 
 import React from 'react';
-import { View, Text, Button } from 'react-native';
-
+import { View, Text, Button, StyleSheet } from 'react-native';
+import exStyles from './styles';
 const App = () => {
 
   return (
     <View>
-      <Child name="Child 1" />
+      <Text style={{ fontSize: 30, color: "orange" }}>This is inline styling</Text>
+      <Text style={styles.textStyle}>This is internal styling</Text>
+      <Text style={exStyles.textStyle}>This is external styling</Text>
     </View>
   );
 }
 
-const Child = (props) => {
-  return (
-    <View>
-      <Text style={{ fontSize: 30 }}>{props.name}</Text>
-    </View>
-  )
-}
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 30,
+    color: "green",
+    textAlign: "center",
+    textAlignVertical: "center",
+    margin: 10,
+    backgroundColor: "yellow",
+    height: 100,
+    width: 300,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "red",
+    fontStyle: "italic",
+    fontWeight: "bold",
 
+  }
+})
 
 
 export default App;
---
+++ styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+const exStyles = StyleSheet.create({
+    textStyle:{
+        fontSize:30,
+        color:"red"
+      }
+})
+
+export default exStyles;
