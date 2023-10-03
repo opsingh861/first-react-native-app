2cf342718bb7df1a3183ca07084604c751ff7168
Aditya Dhanraj <opsingh861@gmail.com>
2023-10-03 20:48:51 +0530

    use of props

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 
-import React, { useState } from 'react';
+import React from 'react';
 import { View, Text, Button } from 'react-native';
 
 const App = () => {
-  var [count, setCount] = useState(0);
+
   return (
     <View>
-      <Text style={{fontSize:30}}>{count}</Text>
-      <Button title="Increase" onPress={() => setCount(count + 1)} />
+      <Child name="Child 1" />
     </View>
   );
+}
+
+const Child = (props) => {
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>{props.name}</Text>
+    </View>
+  )
 }
 
 
