55954f0214a6c608019c32c268d974aebad4040d
Aditya Dhanraj <opsingh861@gmail.com>
2023-10-03 20:46:17 +0530

    using State in the app

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Button } from 'react-native';
 
 const App = () => {
-
+  var [count, setCount] = useState(0);
   return (
     <View>
-      <Text> Hello World </Text>
-      <Button title="Click Me" onPress={() => alert('Button Clicked')} />
+      <Text style={{fontSize:30}}>{count}</Text>
+      <Button title="Increase" onPress={() => setCount(count + 1)} />
     </View>
   );
 }
