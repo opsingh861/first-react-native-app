5026ddb9d7fc08e650e57c7d7008a43e6f82f31b
Aditya Dhanraj <opsingh861@gmail.com>
2023-10-05 19:37:45 +0530

    SectionList in react native

--- App.js
+++ App.js
@@ -1,59 +1,30 @@
 
 import React from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, SectionList } from 'react-native';
 
 
 const App = () => {
   const users = [
-    { name: 'A', age: 20 },
-    { name: 'B', age: 21 },
-    { name: 'C', age: 22 },
-    { name: 'D', age: 23 },
-    { name: 'E', age: 24 },
-    { name: 'F', age: 25 },
-    { name: 'G', age: 26 }
+    { name: "John", age: 30, data: ["c++", "python"] },
+    { name: "Doe", age: 20, data: ["c++", "python", "react"] },
+    { name: "Smith", age: 40, data: ["c++", "python", "react", "react native"] },
+    { name: "Alex", age: 50, data: ["c++", "python", "react", "react native", "node js"] },
+
   ]
   return (
     <View>
-      <Text style={{ fontSize: 20 }}>Component in loop with FlatList</Text>
-      <FlatList
-        data={users}
-        renderItem={({ item }) => <UserData name={item.name} age={item.age} />}
+      <Text style={{ fontSize: 30 }}>Section List in react native</Text>
+      <SectionList
+        sections={users}
+        renderItem={({ item }) => <Text style={{ fontSize: 20, marginLeft:20 }}>{item}</Text>}
+        renderSectionHeader={({ section: { name } }) => (
+          <Text style={{fontSize:25,color:"blue"}}>{name}</Text>
+        )}
       />
     </View>
   );
 };
 
-const UserData = (props) => {
-  const item = props
-  return (
-    <View style={styles.box}>
-      <Text style={styles.text}>{item.name}</Text>
-      <Text style={styles.text}>{item.age}</Text>
-    </View>
-  )
-}
-
-const styles = StyleSheet.create({
-  box: {
-    flex: 1,
-    margin: 5,
-    padding: 10,
-    borderWidth: 3,
-    borderRadius: 10,
-    borderColor: "yellow",
-    flexDirection: "row"
-
-  },
-  text: {
-    color: "orange",
-    fontSize: 30,
-    borderColor: "yellow",
-    flex: 1,
-    textAlign: "center",
-
-  }
-})
 
 
 export default App;
