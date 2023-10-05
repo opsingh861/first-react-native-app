e05c26a1df96326f9db7fed49e5a5b8df2b5d0c5
Aditya Dhanraj <opsingh861@gmail.com>
2023-10-05 21:09:47 +0530

    use of props in class component

--- App.js
+++ App.js
@@ -4,16 +4,11 @@ import { View, Text, StyleSheet, SectionList } from 'react-native';
 import ClassComponent from './ClassComponent';
 
 const App = () => {
-  const users = [
-    { name: "John", age: 30, data: ["c++", "python"] },
-    { name: "Doe", age: 20, data: ["c++", "python", "react"] },
-    { name: "Smith", age: 40, data: ["c++", "python", "react", "react native"] },
-    { name: "Alex", age: 50, data: ["c++", "python", "react", "react native", "node js"] },
-
-  ]
+const name = "hello";
+const item = "item";
   return (
     <View>
-      <ClassComponent />
+      <ClassComponent name={name} item = {item} /> {/* in this props name should be same as variable */}
     </View>
   );
 };
--- ClassComponent.js
+++ ClassComponent.js
@@ -2,21 +2,12 @@ import React, { Component, useState } from "react";
 import { Button, TextInput, Text, View } from "react-native";
 
 class ClassComponent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: ""
-        }
-    } // we cannot use useState in class component
-    name = () => { // we cannot simply use function in class component
-        console.warn(this.state.name);
-    }
     render() {
         return (
             <View>
                 <Text style={{ color: "red", fontSize: 30 }}>Class Component</Text>
-                <TextInput placeholder="Enter your name" onChangeText={(text) => this.setState({ name: text })} />
-                <Button title="Click here" onPress={ this.name} />
+                <Text style={{ color: "red", fontSize: 30 }}>Welcome {this.props.name} {this.props.item} </Text>
+                <Button title="Click here" onPress={ this.props.name} />
             </View>
         )
     }
