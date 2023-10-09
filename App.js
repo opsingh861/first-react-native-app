3f8989e29aa1cab261827dcd06b12a4a58c32aa0
Aditya Dhanraj <opsingh861@gmail.com>
2023-10-09 23:24:49 +0530

    toggle component

--- App.js
+++ App.js
@@ -1,41 +1,28 @@
 
-import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, SectionList, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [data, setData] = useState(0);
+  const [show, setShow] = useState(false);
 
   return (
     <View>
-      <Text style={{ fontSize: 30 }}>Count: {count}</Text>
-      <Text style={{ fontSize: 30 }}>Data: {count}</Text>
-      <Button title='Update Count' onPress={() => setCount(count + 1)} />
-      <Text></Text>
-      <Button title='Update Data' onPress={() => setData(data + 1)} />
-      <Users info={{ count, data }} />
+      <Text style={{ fontSize: 30 }}>Show/Hide Component</Text>
+      <Button title='toggle button' onPress={() => setShow(!show)} />
+      {
+        show ? <Users /> : null
+      }
     </View>
   );
 };
 
-const Users = (props) => {
-  const count = props.info.count;
-  const data = props.info.data;
-  useEffect(() => {
-    console.warn("count changed");
-  }, [count]); // only re-run the effect if count changes, this is componentDidUpdate
-  useEffect(() => {
-    console.warn("data changed");
-  }, [data]);
+const Users = () => {
   return (
     <View>
-      <Text style={{ color: "orange", fontSize: 30 }}>Count: {count}</Text>
-      <Text style={{ color: "orange", fontSize: 30 }}>Data: {data}</Text>
+      <Text style={{ fontSize: 30, color: "green" }}>User Component</Text>
     </View>
   )
 }
-
-
 
 export default App;
