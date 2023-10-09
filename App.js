2d0b1bef8a1c80af51858b5ed0d97c48389e5b00
Aditya Dhanraj <opsingh861@gmail.com>
2023-10-09 20:41:16 +0530

    componentDidUpdate

--- App.js
+++ App.js
@@ -5,19 +5,36 @@ import { View, Text, StyleSheet, SectionList, Button } from 'react-native';
 
 const App = () => {
   const [count, setCount] = useState(0);
-  // useEffect(() => {
-  //   console.warn("hello") // this will run after every render including the updation of the state
-  // })
-  // useEffect(() => {
-  //   console.warn("hello") // this will run only once after the first render
-  // }, []) // this is the dependency array, if we pass an empty array then it will run only once after the first render, we call it as componentDidMount
+  const [data, setData] = useState(0);
+
   return (
     <View>
-      <Text style={{ fontSize: 30 }}>{count}</Text>
-      <Button title='Increment' onPress={() => setCount(count + 1)} />
+      <Text style={{ fontSize: 30 }}>Count: {count}</Text>
+      <Text style={{ fontSize: 30 }}>Data: {count}</Text>
+      <Button title='Update Count' onPress={() => setCount(count + 1)} />
+      <Text></Text>
+      <Button title='Update Data' onPress={() => setData(data + 1)} />
+      <Users info={{ count, data }} />
     </View>
   );
 };
+
+const Users = (props) => {
+  const count = props.info.count;
+  const data = props.info.data;
+  useEffect(() => {
+    console.warn("count changed");
+  }, [count]); // only re-run the effect if count changes, this is componentDidUpdate
+  useEffect(() => {
+    console.warn("data changed");
+  }, [data]);
+  return (
+    <View>
+      <Text style={{ color: "orange", fontSize: 30 }}>Count: {count}</Text>
+      <Text style={{ color: "orange", fontSize: 30 }}>Data: {data}</Text>
+    </View>
+  )
+}
 
 
 
