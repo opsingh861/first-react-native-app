0946b1e7ba60b5429b117a27b1da2012e69faeb3
Aditya Dhanraj <opsingh861@gmail.com>
2023-10-12 19:56:32 +0530

    ActivityIndicator

--- App.js
+++ App.js
@@ -1,42 +1,22 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableHighlight, TouchableOpacity } from 'react-native';
+import { View, Text, ActivityIndicator, StyleSheet, Button } from 'react-native';
 
 const App = () => {
-  const skills = [
-    {
-      id: 1,
-      name: "C++"
-    },
-    {
-      id: 2,
-      name: "Python"
-    },
-    {
-      id: 3,
-      name: "Java"
-    },
-    {
-      id: 4,
-      name: "HTML"
-    },
-  ];
 
-  const [radioSelected, setRadioSelected] = useState(1);
+
+  const [show, setShow] = useState(false);
+  const loader = () => {
+    setShow(true);
+    setTimeout(() => {
+      setShow(false);
+    }, 3000);
+  }
 
   return (
     <View style={styles.main}>
-      <View style={styles.column}>
-        {skills.map((item, index) => (
-          <TouchableOpacity key={item.id} onPress={() => setRadioSelected(item.id)}>
-            <View style={styles.container}>
-              <View style={styles.button}>
-                {radioSelected == item.id ? <View style={styles.internal}></View> : null}
-              </View>
-              <Text style={styles.text}>{item.name}</Text>
-            </View>
-          </TouchableOpacity>
-        ))}
-      </View>
+      <ActivityIndicator size={'large'} color={'green'} animating={show}  /* only large small and medium supports in ios */ />
+      <ActivityIndicator size={60} color={'blue'} animating={show} />
+      <Button title='Load' onPress={loader} />
     </View>
   );
 };
@@ -46,36 +26,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  column: {
-    flexDirection: 'column', // Set flexDirection to 'column' for a vertical alignment
-    alignItems: 'center', // Align items in the center of the column
-    justifyContent: 'center', // Align items to the center of the column
-  },
-  button: {
-    borderColor: 'blue',
-    borderWidth: 2,
-    height: 25,
-    width: 25,
-    borderRadius: 15,
-    marginRight: 5,
-  },
-  container: {
-    justifyContent: "center",
-    alignItems: "center",
-    margin: 8,
-    flexDirection: 'row',
-  },
-  internal: {
-    backgroundColor: 'blue',
-    height: 15,
-    width: 15,
-    borderRadius: 100,
-    margin: 3,
-  },
-  text: {
-    color: 'blue',
-  },
-});
+  }
+})
 
 export default App;
