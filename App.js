542c3773cabcc61235aba61bf738abbd5257e57b
Aditya Dhanraj <opsingh861@gmail.com>
2023-10-13 22:29:41 +0530

    Use of npm to install WebView package

--- App.js
+++ App.js
@@ -1,37 +1,22 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Platform } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview'
 
 const App = () => {
-  const [hidden, sethidden] = useState(false);
-  const [style, setstyle] = useState('default');
+
   return (
-    <View style={styles.main}>
-      <Text style={{ fontSize: 30 }}>Platform: {Platform.OS}</Text>
-      <Text style={{ fontSize: 30 }}>Version: {Platform.Version}</Text>
-      <Text style={{ fontSize: 30 }}>isTV: {Platform.isTV ? 'true' : 'false'}</Text>
-      {Platform.os == "android" ? <View style={styles.android}></View> : <View style={styles.ios}></View>}
-      <Text style={{ fontSize: 15 }}>{JSON.stringify(Platform)}</Text>
-    </View>
+    <WebView style={styles.main} source={{ uri: 'https://www.google.com' }} />
   );
 };
 
 const styles = StyleSheet.create({
   main: {
     flex: 1,
-    justifyContent: 'center',
+    // justifyContent: 'center',
     // alignItems: 'center'
-  },
-  android: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'blue'
-  },
-  ios: {
-    marginTop: 20,
-    width: 100,
-    height: 100,
-    backgroundColor: 'red'
+    margin:10
   }
+
 })
 
 export default App;
