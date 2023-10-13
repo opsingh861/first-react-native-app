029a27039c29b5f3f3aeaef3c19b9c05c375f76c
Aditya Dhanraj <opsingh861@gmail.com>
2023-10-13 22:03:11 +0530

    StatusBar

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { View, Text, StyleSheet, StatusBar, Button } from 'react-native';
 
 const App = () => {
-
+  const [hidden, sethidden] = useState(false);
+  const [style, setstyle] = useState('default');
   return (
     <View style={styles.main}>
-      <Pressable
-      onPress={()=>console.warn("Normal press")}
-      onLongPress={()=>console.warn("Long press")}
-      delayLongPress={3000}
-      onPressIn={()=>console.warn("Press in")}
-      onPressOut={()=>console.warn("Press out")}
-      >
-        <Text style={styles.pressableBtn}>Pressable</Text>
-      </Pressable>
+      <StatusBar backgroundColor="blue" barStyle={style} hidden={hidden} />
+      <Button title="toggle" onPress={() => sethidden(!hidden)} />
+      <Button title="Show" onPress={() => setstyle('dark-content')} />
+      
     </View>
   );
 };
@@ -22,20 +18,8 @@ const styles = StyleSheet.create({
   main: {
     flex: 1,
     justifyContent: 'center',
-    margin: 10,
-  },
-  pressableBtn: {
-    backgroundColor: 'blue',
-    padding: 10,
-    margin: 10,
-    borderRadius: 10,
-    shadowColor: 'black',
-    elevation: 5,
-    color: "#fff",
-    textAlign: 'center',
-    fontSize: 20
+    // alignItems: 'center'
   }
-
 })
 
 export default App;
